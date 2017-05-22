e5d4d29947ed781968262397dc6ac458a29b4c35
25hours <shawnniu@yeah.net>
2017-05-23 07:34:58 +0800

    design database models done

--- Robb/admin.py
+++ Robb/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
-
+from Robb import models
 # Register your models here.
+
+class HostAdmin(admin.ModelAdmin):
+    list_display = ('id','ip_addr','status')
+    filter_horizontal = ('host_group','templates')
+
+class TemplateAdmin(admin.ModelAdmin):
+    filter_horizontal = ('services','triggers')
+
+class ServiceAdmin(admin.ModelAdmin):
+    filter_horizontal = ('items',)
+    list_display = ('name','interval','plugin_name')
+
+class TriggerExpressionInline(admin.ModelAdmin):
+    model = models.TriggerExpression
+
+class TriggerAdmin(admin.ModelAdmin):
+    list_display = ('name','severity','enabled')
+    inlines = [TriggerExpressionInline]
+
+class TriggerExpressionAdmin(admin.ModelAdmin):
+    list_display = ('trigger','service','service_index','specified_index_key')
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','name','phone','weixin','email')
+
+admin.site.register(models.Host,HostAdmin)
+admin.site.register(models.HostGroup)
+admin.site.register(models.Template,TemplateAdmin)
+admin.site.register(models.Service,ServiceAdmin)
+admin.site.register(models.Trigger,TriggerAdmin)
+admin.site.register(models.TriggerExpression,TriggerExpressionAdmin)
+admin.site.register(models.ServiceIndex)
+admin.site.register(models.Action)
+admin.site.register(models.ActionOperation)
+admin.site.register(models.Maintenance)
--- Robb/models.py
+++ Robb/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 # Create your models here.
 
 class Host(models.Model):
@@ -141,7 +143,7 @@ class ActionOperation(models.Model):
         ('sms','SMS'),
         ('script','RunScript'),
     )
-    action_type = models.CharField(u'动作类型',choices=action_type_choices)
+    action_type = models.CharField(u'动作类型',choices=action_type_choices,max_length=64)
     notifiers = models.ManyToManyField('UserProfile',verbose_name=u'通知对象',blank=True,max_length=64)
     _msg_format = '''Host({hostname},{ip}) service({service_name}) has issue,msg:{msg}'''
 
@@ -160,3 +162,66 @@ class Maintenance(models.Model):
 
     def __str__(self):
         return self.name
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, name, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class UserProfile(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    name = models.CharField(max_length=32)
+    #date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    objects = MyUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+    def __str__(self):              # __unicode__ on Python 2
+        return self.email
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
