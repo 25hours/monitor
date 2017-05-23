127cccf73143972588c3e787dab6cb77baf12425
25hours <shawnniu@yeah.net>
2017-05-23 08:43:59 +0800

    add monitor client

---
+++ Robb/Theon/bin/theon.py
@@ -0,0 +1,8 @@
+import os,sys
+from core import main
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+if __name__ == "__main__":
+    client = main.command_handler(sys.argv)
