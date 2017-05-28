3801a4e0d0fb674000abc2ad8971367d11aed6f0
25hours <shawnniu@yeah.net>
2017-05-28 08:16:55 +0800

    front page

--- Robb/urls.py
+++ Robb/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import url,include
 from Robb import views
 urlpatterns = [
-    url(r'api/',include('Robb.rest_urls'))
+    url(r'api/',include('Robb.rest_urls')),
+    url(r'^$', views.index),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^triggers/$', views.triggers, name='triggers'),
+    url(r'hosts/$', views.hosts, name='hosts'),
+    url(r'hosts/(\d+)/$', views.host_detail, name='host_detail'),
+    # url(r'graph/$',views.graph ,name='get_graph'),
+    url(r'trigger_list/$', views.trigger_list, name='trigger_list'),
 ]
--- Robb/views.py
+++ Robb/views.py
@@ -10,6 +10,29 @@ from Robb import models
 
 # Create your views here.
 REDIS_OBJ = redis_conn.redis_conn(settings)
+def dashboard(request):
+
+    return render(request,'Robb/monitor/dashboard.html')
+def triggers(request):
+
+    return render(request,'Robb/monitor/triggers.html')
+def index(request):
+    return render(request,'Robb/monitor/index.html')
+def hosts(request):
+    host_list = models.Host.objects.all()
+    print("hosts:",host_list)
+    return render(request,'Robb/monitor/hosts.html',{'host_list':host_list})
+def host_detail(request,host_id):
+    host_obj = models.Host.objects.get(id=host_id)
+    return render(request,'Robb/monitor/host_detail.html',{'host_obj':host_obj})
+def trigger_list(request):
+
+    host_id = request.GET.get("by_host_id")
+
+    host_obj = models.Host.objects.get(id=host_id)
+
+    alert_list = host_obj.eventlog_set.all().order_by('-date')
+    return render(request,'Robb/monitor/trigger_list.html',locals())
 def client_configs(requset,client_id):
     print("--->",client_id)
     config_obj = ClientHandler(client_id)
